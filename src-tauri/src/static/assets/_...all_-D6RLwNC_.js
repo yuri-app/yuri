8a992b3db69f34eec4e9ed218523df1010f066be
hanaTsuk1 <101488209+hanaTsuk1@users.noreply.github.com>
2024-02-27 10:04:20 +0800

    static

---
+++ src-tauri/src/static/assets/_...all_-D6RLwNC_.js
@@ -0,0 +1,1 @@
+import{d as e,e as o}from"./index-B5zRwVpF.js";const n=e({__name:"[...all]",setup(r){return o().push("/404"),()=>{}}});export{n as default};
--- src-tauri/src/static/assets/_path_-C-15zolB.js
+++
@@ -1,1 +0,0 @@
-import{d as s,o as c,c as n}from"./index-BXj2hsl3.js";const o=["src"],_=s({__name:"[path]",props:{path:{}},setup(e){const t=e;return(r,a)=>(c(),n("iframe",{src:t.path,"h-screen":"","w-screen":""},null,8,o))}});export{_ as default};
